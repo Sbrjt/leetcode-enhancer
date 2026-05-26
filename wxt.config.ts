0d0b836e6de75cf8b77e9faa75f484d2ec8f6884
sbrjt <champa.281982@gmail.com>
2026-05-27 00:03:03 +0530

    chore: use .env vars

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,33 +1,34 @@
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'node:path'
+import { env } from 'process'
 import { defineConfig } from 'wxt'
 
 export default defineConfig({
-	manifest: {
-		web_accessible_resources: [
-			{
-				resources: ['patchMonaco.js'],
-				matches: ['https://leetcode.com/*'],
-				use_dynamic_url: true,
+	manifest: () => {
+		const { CHROME_PUBLIC_KEY, FIREFOX_EXTENSION_ID } = env
+
+		return {
+			web_accessible_resources: [
+				{
+					resources: ['patchMonaco.js'],
+					matches: ['https://leetcode.com/*'],
+					use_dynamic_url: true,
+				},
+			],
+			permissions: ['search', 'storage'],
+			key: CHROME_PUBLIC_KEY,
+			browser_specific_settings: {
+				gecko: {
+					id: FIREFOX_EXTENSION_ID,
+					data_collection_permissions: { required: ['none'] },
+				},
 			},
-		],
-		permissions: ['search', 'storage'],
-		browser_specific_settings: {
-			gecko: {
-				id: 'leetcode-enhancer@sbrjt',
-				data_collection_permissions: { required: ['none'] },
-			},
-		},
+		}
 	},
 	modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
 	srcDir: 'src',
-	// entrypointsDir: 'src',
-	autoIcons: {
-		baseIconPath: resolve('public/icon.svg'),
-	},
-	vite: () => ({
-		plugins: [tailwindcss()],
-	}),
+	autoIcons: { baseIconPath: resolve('public/icon.svg') },
+	vite: () => ({ plugins: [tailwindcss()] }),
 	webExt: {
 		chromiumProfile: resolve('.wxt/chrome-data'),
 		keepProfileChanges: true,
