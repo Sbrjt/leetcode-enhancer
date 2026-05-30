6beac3945d778be76eefee374a886adb414bcf69
sbrjt <champa.281982@gmail.com>
2026-05-30 23:31:21 +0530

    feat: add company tags

---
+++ src/entrypoints/content/hooks/useCompany.ts
@@ -0,0 +1,57 @@
+// add company tag buttons
+
+import { useStorageItem } from '@/hooks/useStore'
+import type { Question } from '@/types'
+import { makeSignal } from '@/utils/lib'
+import elementReady from 'element-ready'
+
+export default function useCompany(question: Question | null) {
+	const [isEnabled, _] = useStorageItem<boolean>('premiumTags')
+
+	useEffect(() => {
+		if (isEnabled === false || question == null) return
+
+		const { controller, signal } = makeSignal()
+
+		let injected: HTMLElement | undefined
+
+			//
+		;(async () => {
+			const div = await elementReady(
+				'div:has(> img[alt="premium lock icon"])',
+				{ signal, stopOnDomReady: false },
+			)
+
+			if (!div) return
+
+			const companies = await getCompanies(question.id)
+
+			if (!companies.length) return
+
+			injected = div.parentElement!.cloneNode() as HTMLElement
+
+			for (const company of companies) {
+				const pill = div.cloneNode() as HTMLElement
+				pill.classList.add('border')
+
+				const img = document.createElement('img')
+				img.width = img.height = 12
+				img.src = `https://img.loadlogo.com/name/${company}`
+
+				// Alternate: `https://logos-api.apistemic.com/linkedin:${company.toLowerCase()}`
+
+				pill.appendChild(img)
+				pill.appendChild(document.createTextNode(company))
+				injected.append(pill)
+			}
+
+			if (signal.aborted) return
+			div.parentElement!.after(injected)
+		})()
+
+		return () => {
+			controller.abort()
+			injected?.remove()
+		}
+	}, [question, isEnabled])
+}
